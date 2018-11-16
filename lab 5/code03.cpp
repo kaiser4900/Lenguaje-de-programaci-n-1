35e8b8a026c101d7a8a5f502de7af4bfa4519575
Unknown <galemanz@ulasalle.edu.pe>
2018-11-16 09:54:59 -0500

    lab 5

---
+++ lab 5/code03.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+struct Date {
+
+    int y;
+    int m;
+    int d;
+};
+
+void init_day(Date& dd, int y, int m, int d)
+{
+
+void add_day(Date& dd, int n)
+{
+
+int main()
+{
+    Date hoy;
+    init_day(hoy, 12, 24, 2005);
+    add_day(today,1);
+    return 0;
+}
