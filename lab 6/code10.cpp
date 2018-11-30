db3ba35e88b7a77df7d82172d3a2c846025cd170
Unknown <galemanz@ulasalle.edu.pe>
2018-11-30 08:05:36 -0500

    lab 6

--- lab 6/code10.cpp
+++ lab 6/code10.cpp
@@ -18,3 +18,14 @@ public:
 
     void set(int n, double v) { elem[n]=v; }
 };
+
+int main()
+{
+    vector v(5);
+
+    for(int i= 0; i<v.size();++i)
+    {
+        v.size()(i,1.1*i);
+        cout<<"v["<<i<<"] == "<<v.getc(i)<<'\n';
+    }
+}
