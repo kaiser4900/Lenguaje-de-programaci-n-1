1e4d077e2390bf0d4078e9ce2fe4b0369032d35c
Unknown <galemanz@ulasalle.edu.pe>
2018-12-07 17:30:43 -0500

    lab 6

--- lab 6/code17.cpp
+++ lab 6/code17.cpp
@@ -2,45 +2,49 @@
 
 using namespace std;
 
-class Link{
-
+class Link
+{
 public:
     string value;
-    Link(const string& v, Link* p 0 0, Link* s=0)
-        :value(v), prev(p),, succ(s) {}
+    Link(const string& v,Link* p=0,Link* s=0)
+        :value(v),prev(p),succ(s){}
     Link* insert(Link* n);
-    Link erase();
-    Link find(const string& s);
-    const Link* find(const string& s) const;
-    Link* next() const { return succ;}
-    Link* previus() const {return prev();}
+    Link* erase();
+    Link* find(const string& s);
+    const Link* find(const string& s)const;
+    Link* next() const{return succ;}
+    Link* previous() const{return prev;}
+    void imprimir();
 private:
     Link* prev;
     Link* succ;
-
 };
 
-Link* Link:: insert(Link* n)
+Link* Link::insert(Link* n)
 {
-    if(n==0) return this;
-    if(this ==0) return n;
-    n->succ = this;
-    if (this->prev)this ->prev->succ = n;
-    n-> prev= this -> prev;
-
-    this -> prev= n;
-
+    if(n==0)return this;
+    if(this==0)return n;
+    n->succ=this;
+    if(this ->prev) this->prev->succ=n;
+    n->prev=this->prev;
+    this->prev=n;
     return n;
 }
-
-Link* Link :: insert(Link* n)
+void Link::imprimir()
 {
-    Link* p=this;
-    if(n==0) return p;
-    if(p==0) return n;
-    n->succ=p;
-    if (p->prev) p->prev->succ = n;
-    n->prev = p-> prev;
-    p->prev = nN
-    ret n;
+Link*o=this;
+while (o)
+{
+cout<<o->value<<" ";
+o=o->succ;
+}
+}
+int main()
+{
+    Link* p = new Link("Hola");
+    Link* q = new Link("juan");
+    Link* r = new Link(":v");
+    q=q->insert(p);
+    q=q->insert(r);
+    q->imprimir();
 }
