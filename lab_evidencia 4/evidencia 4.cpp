1abd93ca07c849db858c9653f8809a869fdce690
Unknown <galemanz@ulasalle.edu.pe>
2018-12-08 03:03:20 -0500

    lab evidencia 4

--- lab_evidencia 4/evidencia 4.cpp
+++ lab_evidencia 4/evidencia 4.cpp
@@ -1,40 +1,55 @@
 #include<iostream>
 using namespace std;
 
-
+//no sé por qué manda error :,v si mañana no me logro despertar para corregirlo le pido su comprensión :,v no estoy seguro si aparece la hora de subida
 int main(){
 
-char* findx(const char*s, const char* x)
-{
-    int i=0;
-    int* res=0;
-    while(s[i])
+char* findX(const char* s, const char* x) {
+    int res=0;
+    if (x==0) return s;
+    for (int i = 0; s[i]; ++i)
     {
-        if(s[i]==*x)
-        {
-            res=s[i];
-            i++;
-        }
+        if (s[i] == x[0])
+            {
+                for (int j = 1; x[j]; ++j)
+                 {
+                    if (x[j+1]==0)
+                    {
+                        res=&s[i];
+                        return res;
+                    }
+
+
+                    if (s[i+j+1]==0)
+                    {
+                        return res;
+                    }
+
+                }
+            }
     }
     return res;
 }
-void to_lower(char* s)
-{
+void to_lower(char* s) {
     int A='A';
+    int Z='Z';
     int a='a';
-    int dif=0;
+    int dif= A-a;
 
-    dif=A-a;
-    int i=0;
-    while(s[i])
-    {
-        *s[i=*s[i]-dif;
+    for (int i = 0; s[i]; ++i) {
+        if (s[i]>=A && s[i]<=Z)
+        {
+            s[i] =s[i]+dif;
+        }
+
     }
 }
+
+
 bool find(string p)
 {
     bool res=false;
-    Link=*r;
+    Link* r;
     r=this;
     while(r)
     {
